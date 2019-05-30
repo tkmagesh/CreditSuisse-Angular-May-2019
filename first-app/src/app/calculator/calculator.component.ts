d78176591fd60ada2ccd72a7d11714a0d2e540d6
Magesh <tkmagesh77@gmail.com>
2019-05-30 12:34:15 +0530

    using $event

--- first-app/src/app/calculator/calculator.component.ts
+++ first-app/src/app/calculator/calculator.component.ts
@@ -9,10 +9,23 @@ export class CalculatorComponent{
 
 	result : number = 0;
 
-	onAddClick(v1 : string, v2 : string){
-		var n1 = parseInt(v1),
-			n2 = parseInt(v2);
-		this.result = n1 + n2;
+	v1 : number = 0;
+	v2 : number = 0;
+
+	/*setV1(value : string){
+		this.v1 = parseInt(value);
+	}
+
+	setV2(value : string){
+		this.v2 = parseInt(value);
+	}*/
+
+	onAddClick(){
+		this.result = this.v1 + this.v2;
+	}
+
+	onSubtractClick() : void {
+		this.result = this.v1 - this.v2;
 	}
 
 }
