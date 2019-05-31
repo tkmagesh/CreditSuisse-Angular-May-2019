f7854dad5bc272c2d3772aefb3a2277e6b1f3ca5
Magesh <tkmagesh77@gmail.com>
2019-05-31 12:54:10 +0530

    using localStorage - uuid

--- bug-tracker-app/src/main.ts
+++ bug-tracker-app/src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as uuid from 'uuid/v4';
+
+window['uuid'] = uuid;
 
 if (environment.production) {
   enableProdMode();
